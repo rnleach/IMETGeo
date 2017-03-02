f013e0c971306c4f58855a17cb69edff383b4579
Ryan Leach <ryan.n.leach@gmail.com>
2017-03-01 21:57:27 -0700

    Finished refactor of range ring calculation.

--- src/RangeRing.cpp
+++ src/RangeRing.cpp
@@ -1,4 +1,5 @@
 #include "RangeRing.hpp"
+#include "OGR_RangeRing.hpp"
 #include "Feature.hpp"
 #include "LineFeature.hpp"
 #include "PointFeature.hpp"
@@ -24,37 +25,12 @@ namespace PFB
 
     // Put the center point on the list.
     toRet.push_back(FP(new PointFeature(name_, color, pnt_.latitude, pnt_.longitude, dispThresh)));
+    OGR_RangeRing rr{ pnt_ };
 
     // Put the rings on the list
-    const double earthRadius = 3959.0; // miles
-    const double PI = 3.14159;
     for(auto rng: ranges_)
     {
-      const size_t NUM_POINTS = 360; // Number of line segments to use to make up ring.
-      vector<point> pnts{};
-      pnts.reserve(NUM_POINTS + 1);
-      double deltaBearing = 360.0 / (NUM_POINTS) / 180.0 * PI; // radians
-      for(size_t i = 0; i < NUM_POINTS + 1; i++)
-      {
-        const double bearing = i * deltaBearing; // radians
-        const double delta = rng/earthRadius;
-
-        const double oldLat = pnt_.latitude * PI / 180.0; // radians
-        const double oldLon = pnt_.longitude * PI / 180.0; // radians
-
-        //
-        // SIMPLIFICATION - assume we'll never be near a pole, cause there is no radar their, so why
-        // make a placefile.
-        //
-        double newLat = asin(sin(oldLat)*cos(delta) + cos(oldLat)*sin(delta)*cos(bearing));
-        double newLon = fmod(oldLon - asin(sin(bearing)*sin(delta)/cos(newLat)) + PI, 2*PI) - PI;
-
-        // Convert back to degrees
-        newLat *= 180.0 / PI;
-        newLon *= 180.0 / PI;
-
-        pnts.push_back(point(newLat, newLon));
-      }
+      vector<point> pnts = rr.getClosedRingForRange(rng);
 
       toRet.push_back(FP(new LineFeature(name_, color, pnts, dispThresh, lineWidth)));
     }
